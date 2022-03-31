552381ec93f8f162e2f6bdfc65a5c176c1465827
Trần Huy Hoàng <wujor01@gmail.com>
2022-03-31 08:45:57 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,86 +1,98 @@
+/* eslint-disable prettier/prettier */
 var axios = require('axios');
 
 var MongoClient = require('mongodb').MongoClient;
 var url = process.env.MONGODB_CONNECTION;
 
-async function MongoInsert(obj, collection = "message") {
+async function MongoInsert(obj, collection = 'message') {
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+  }).catch((err) => {
+    console.log(err);
+  });
 
-  const client = await MongoClient.connect(url, { useNewUrlParser: true })
-        .catch(err => { console.log(err); });
+  if (!client) {
+    return;
+  }
 
-    if (!client) {
-        return;
-    }
-
-    try {
-        var dbo = client.db("mydb");
-        let res = await dbo.collection(collection).insertOne(obj);
-        return res;
-    } catch (err) {
-
-        console.log(err);
-    } finally {
-
-        client.close();
-    }
+  try {
+    var dbo = client.db('mydb');
+    let res = await dbo.collection(collection).insertOne(obj);
+    return res;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    client.close();
+  }
 }
 
-async function MongoFindQuery(query, collection = "bubble", fieldsRemove = {_id:0}) {
-  const client = await MongoClient.connect(url, { useNewUrlParser: true })
-        .catch(err => { console.log(err); });
+async function MongoFindQuery(
+  query,
+  collection = 'bubble',
+  fieldsRemove = { _id: 0 }
+) {
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+  }).catch((err) => {
+    console.log(err);
+  });
 
-    if (!client) {
-        return;
-    }
+  if (!client) {
+    return;
+  }
 
-    try {
-
-        var dbo = client.db("mydb");
-        let res = await dbo.collection(collection).find(query).project(fieldsRemove).toArray();
-        return res;
-
-    } catch (err) {
-
-        console.log(err);
-    } finally {
-
-        client.close();
-    }
+  try {
+    var dbo = client.db('mydb');
+    let res = await dbo
+      .collection(collection)
+      .find(query)
+      .project(fieldsRemove)
+      .toArray();
+    return res;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    client.close();
+  }
 }
 
-async function CallAPILine(method = 'get', url = 'https://api.line.me/v2/bot/profile/Uf072abc9505c04336bb29af8ae9c1a11') {
+async function CallAPILine(
+  method = 'get',
+  url = 'https://api.line.me/v2/bot/profile/Uf072abc9505c04336bb29af8ae9c1a11'
+) {
   try {
     var config = {
       method: method,
       url: url,
-      headers: { 
-        'Authorization': `Bearer ${process.env.LINE_ACCESS_TOKEN}`
-      }
+      headers: {
+        Authorization: `Bearer ${process.env.LINE_ACCESS_TOKEN}`,
+      },
     };
-    
+
     return await axios(config);
   } catch (error) {
     console.log(error);
   }
 }
 
-module.exports = async function App(context) { 
-
+module.exports = async function App(context) {
   if (context.event.isText) {
     var inputText = context.event.text.toLowerCase();
 
     switch (inputText) {
       case 'chào':
-        var res = await CallAPILine('get',`https://api.line.me/v2/bot/profile/${context.session.user.id}`);
+        var res = await CallAPILine(
+          'get',
+          `https://api.line.me/v2/bot/profile/${context.session.user.id}`
+        );
         await context.sendText(`Chao xìn ${res.data.displayName}!`);
         break;
       case 'menu':
-        let listBundle = await MongoFindQuery({}, "bubble");
-        console.log(listBundle);
-
-        let bodySend = {};
-        bodySend.type = "carousel";
-        bodySend.contents = listBundle;
+        var listBundle = await MongoFindQuery({}, 'bubble');
+        var bodySend = {
+          type: 'carousel',
+          contents: listBundle,
+        };
 
         await context.sendFlex('Menu', bodySend);
         break;
