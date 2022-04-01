0d29b1e855d5ca33118d337ddff88711742eba48
Trần Huy Hoàng <wujor01@gmail.com>
2022-04-01 15:41:49 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,53 @@ var MongoClient = require('mongodb').MongoClient;
 var _ = require('lodash');
 var url = process.env.MONGODB_CONNECTION;
 
+//#region Hàm +- ngày tháng
 Date.prototype.addHours = function(h) {
   this.setTime(this.getTime() + (h*60*60*1000));
   return this;
 }
+
+Date.prototype.addDays = function(days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
+
+Date.prototype.addMonths = function (value) {
+  var n = this.getDate();
+  this.setDate(1);
+  this.setMonth(this.getMonth() + value);
+  this.setDate(Math.min(n, this.getDaysInMonth()));
+  return this;
+};
+
+Date.isLeapYear = function (year) { 
+  return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0)); 
+};
+
+Date.getDaysInMonth = function (year, month) {
+  return [31, (Date.isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
+};
+
+Date.prototype.isLeapYear = function () { 
+  return Date.isLeapYear(this.getFullYear()); 
+};
+
+Date.prototype.getDaysInMonth = function () { 
+  return Date.getDaysInMonth(this.getFullYear(), this.getMonth());
+};
+
+Date.prototype.toVNDateString = function () { 
+  const yyyy = this.getFullYear();
+  let mm = this.getMonth() + 1; // Months start at 0!
+  let dd = this.getDate();
+
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+
+  return dd + '/' + mm + '/' + yyyy;
+};
+//#endregion
 
 async function MongoInsert(obj, collection = 'message') 
 {
@@ -92,6 +135,22 @@ async function GetOrderInDay(dateNow, ispaid, lineid){
 
   return await MongoFindQuery(objFilter, "order",{});
 }
+
+async function GetOrderInMonth(year, month, ispaid, lineid){
+  var objFilter = {
+      $and: [
+      {createddate : {$gte : new Date(year, month, 1)}},
+      {createddate : {$lt : new Date(year, month, 1).addMonths(1).addDays(-1)}},
+      {ispaid : ispaid},
+      {lineid : lineid}
+    ]
+  };
+
+  console.log(JSON.stringify(objFilter));
+
+  return await MongoFindQuery(objFilter, "order",{});
+}
+
 
 async function ConfirmOrder(dateNow, userid, username, lineid) 
 {
@@ -262,6 +321,39 @@ module.exports = async function App(context) {
 
           break;
         default:
+          
+          //#region ds order theo tháng
+          if(inputText.indexOf('order') > -1){
+            var yearmonth = inputText.split('order')[1].trim();
+
+            var year = yearmonth.slice(0,4);
+            var month = yearmonth.slice(4,6);
+
+            if(!year || !month){
+              await context.sendText(`Năm tháng không đúng định dạng (yyyyMM)!`);
+              return;
+            }
+
+            var listorder = await GetOrderInMonth(year, month, true, context.session.id);
+            var txt = '';
+
+            listorder.forEach(item => {
+              txt += `${item.createddate.toVNDateString()}: ${item.user.username} order ${item.product.productname} giá ${item.product.price.toLocaleString('vi-VN',{style: 'currency', currency: 'VND'})}`
+            });
+
+            if (!txt) {
+              await context.sendText(`Không có order nào trong tháng ${month}-${year}`);
+              return;
+            }
+
+            txt = `DS order coofee tháng ${month}-${year}\n` + txt;
+            var totalMoney = listorder.reduce((a,curr) => a + curr.payment, 0);
+            txt += `Tổng tiền thanh toán: ${totalMoney.toLocaleString('vi-VN',{style: 'currency', currency: 'VND'})}`;
+            await context.sendText(txt);
+            return;
+          }
+          //#endregion
+
           var data = await MongoFindQuery({productname: inputText}, "product",{});
   
           if (data[0]) {
