d1ff39cf31d34fa01590f6dc45be98e6776ab390
Trần Huy Hoàng <wujor01@gmail.com>
2022-04-02 16:42:07 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -400,7 +400,7 @@ module.exports = async function App(context) {
           if(numOrder == 0)
             await context.sendText('Bạn không chưa có order nào trong ngày hôm nay chưa thanh toán');
           else
-            await context.sendText(`Xóa thành công ${numOrder} order trong ngày hôm nay`);
+            await context.sendText(`Xóa thành công ${numOrder} order của bạn trong ngày hôm nay`);
 
           break;
         default:
